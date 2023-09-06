01fbee7edc762786ef1f91e454323e55aef58ed3
FrontEndDanni <dannikabdull@gmail.com>
2023-09-06 16:37:24 -0700

    ui adjustments and display toggle for projects

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AboutMe from './components/AboutMe';
 import Project from './components/Project';
 import WorkExperience from './components/WorkExperience';
@@ -7,15 +7,20 @@ import Footer from './components/Footer'; // Import Footer component
 import projectsData from './data/projects';
 
 function App() {
+  const [showAllProjects, setShowAllProjects] = useState(false);
+
+  // Define the number of projects to display
+  const numProjectsToShow = showAllProjects ? projectsData.length : 3;
+
   return (
     <div className="App">
-      <Navbar /> {/* Add the Navbar component */}
+      <Navbar />
       <h1>Dannika Dull</h1>
       <AboutMe />
       <WorkExperience positions={projectsData.slice(-3)} />
       <h2>My Projects</h2>
       <div className="projects-list">
-        {projectsData.map((project, index) => (
+        {projectsData.slice(0, numProjectsToShow).map((project, index) => (
           <Project
             key={index}
             title={project.title}
@@ -24,7 +29,16 @@ function App() {
           />
         ))}
       </div>
-      <Footer /> {/* Add the Footer component */}
+      {showAllProjects ? (
+        <button
+        className="button"
+        onClick={() => setShowAllProjects(false)}>Minimize Projects</button>
+      ) : (
+        <button 
+        className="button"
+        onClick={() => setShowAllProjects(true)}>View All Projects</button>
+      )}
+      <Footer />
     </div>
   );
 }

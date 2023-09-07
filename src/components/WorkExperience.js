ab97fdf62199770fdbc554275eac105459f7f912
FrontEndDanni <dannikabdull@gmail.com>
2023-09-06 21:41:21 -0700

    fixed resume download button

--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -1,28 +1,25 @@
 import React from 'react';
 import '../App.css';
 import workExperienceData from '../data/workExperience';
-import ResumeDownloadButton from './ResumeDownloadButton'; 
-
+import ResumeDownloadButton from './ResumeDownloadButton';
 
 const WorkExperience = () => {
-    return (
-      <div className="WorkExperience">
-        <h2>Work Experience</h2>
-        <div className="positions">
-          {workExperienceData.map((position, index) => (
-            <div className="position" key={index}>
-              <h3>{position.title}</h3>
-              <p>{position.company}</p>
-              <p>{position.date}</p>
-            </div>
-          ))}
-        </div>
-        <ResumeDownloadButton
-        pdfUrl="/path-to-your-resume.pdf" // Provide the URL to your resume PDF
-        fileName="Dannika_Dull_Resume.pdf" // Specify the desired file name
-      />
+  const pdfUrl = '/pdf/Dannika Dull.pdf'; 
+  return (
+    <div className="WorkExperience">
+      <h2>Work Experience</h2>
+      <div className="positions">
+        {workExperienceData.map((position, index) => (
+          <div className="position" key={index}>
+            <h3>{position.title}</h3>
+            <p>{position.company}</p>
+            <p>{position.date}</p>
+          </div>
+        ))}
       </div>
-    );
-  };
-  
-  export default WorkExperience;
+      <ResumeDownloadButton pdfUrl={pdfUrl} fileName="Dannika_Dull.pdf" />
+    </div>
+  );
+};
+
+export default WorkExperience;
